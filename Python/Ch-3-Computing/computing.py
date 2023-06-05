8ceb578d95a2414496137a6b6fc3d61f7cd536b3
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-06-05 19:51:25 +0500

    add example for logical AND and OR

--- Python/Ch-3-Computing/computing.py
+++ Python/Ch-3-Computing/computing.py
@@ -22,3 +22,13 @@ max_number_of_players = 1.024e18
 print(max_number_of_players)
 # Output in decimal notation
 # 1,024,000,000,000,000,000
+
+print("---------- Logical Operators in Python ----------")
+print("True and True is " + str(True and True) + " Because Logical AND requires both side to be True")
+# True
+print("True and False is " + str(True and False) + " Because Logical AND requires both side to be True")
+# False
+print("True or False is " + str(True or False) + " Because Logical OR requires atleast one input to be True")
+# True
+print("False or False is " + str(False or False) + " Because Logical OR requires atleast one input to be True")
+# False

82ec6fef13fdeb233fb686097861afb030f6f0cb
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-06-05 19:32:17 +0500

    revisiting concepts for python

---
+++ Python/june2023/1-revision.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+number = 12
+print("Number is " + str(number))
+print("Type of number variable is " + str(type(number)))
+
+print("-----------------------------------------")
+print("This is multi-variables declaration")
+
+name, age, profession = "Abdullah", 32, "Developer"
+print(name, age, profession)
+print("-----------------------------------------")
